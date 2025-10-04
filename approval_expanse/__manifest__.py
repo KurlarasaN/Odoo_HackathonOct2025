af58994a4cddb408fb8b508a8c739e7f2cd5fe7b
Keerthana <keerthana@srikeshinfotech.com>
2025-10-04 09:30:28 +0530

    Initial Commit

---
+++ approval_expanse/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Expanse Management',
+    'version': '1.0',
+    'summary': '',
+    'author': 'Crewxdev',
+    'license': "OPL-1",
+    'website': '',
+    'description': """ """,
+    'depends': ['website','hr'],
+    'data': [
+             ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
