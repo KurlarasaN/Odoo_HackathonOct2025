86566787f8626b09861de1901a51f174a74400b6
Kuralarasan <kuralarasan@srikeshinfotech.com>
2025-10-04 11:07:00 +0530

    Reset password

---
+++ approval_expense/controllers/main.py
@@ -0,0 +1,30 @@
+from odoo import http
+from odoo.http import request
+
+class EmployeePortal(http.Controller):
+
+    @http.route('/my/employees', type='http', auth='user', website=True)
+    def portal_my_employees(self, **kwargs):
+        # Fetch all employees
+        employees = request.env['hr.employee'].sudo().search([])
+        return request.render('approval_expense.portal_my_employees_template', {
+            'employees': employees,
+        })
+
+    @http.route('/create_employee', type='http', auth='user', methods=['POST'], website=True)
+    def create_employee(self, **post):
+        # Get form data
+        name = post.get('name')
+        manager_id = post.get('manager_id')
+
+        # Convert manager_id to int if exists
+        manager_id = int(manager_id) if manager_id else False
+
+        # Create new employee
+        request.env['hr.employee'].sudo().create({
+            'name': name,
+            'parent_id': manager_id
+        })
+
+        # Redirect back to employee list
+        return request.redirect('/my/employees')
